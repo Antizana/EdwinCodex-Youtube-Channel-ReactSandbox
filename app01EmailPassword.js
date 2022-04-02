5b054ad4174b08b73df83da284a7d3a6ab34ce12
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-04-01 21:40:06 -0500

    first commit

---
+++ app01EmailPassword.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [submitted, setSubmitted] = useState(false);
+
+  const inputHandler = (event) => {
+    const { name, value } = event.target;
+    setSubmitted(false);
+    if (name === "email") setEmail(value);
+    else setPassword(value);
+  };
+
+  const submitHandler = (event) => {
+    setSubmitted(true);
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input type="text" name="email" onChange={inputHandler} />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <br />
+        <input type="text" name="password" onChange={inputHandler} />
+        <br />
+        <input type="submit" onClick={submitHandler} />
+      </form>
+      {submitted && (
+        <div>
+          Email: {email}
+          <br />
+          Password: {password}
+        </div>
+      )}
+    </>
+  );
+}
